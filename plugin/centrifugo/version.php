5cf4c748a4b4d0add002a1becbb247c4aeb8bce2
Marina Glancy <marina.glancy@gmail.com>
2026-02-24 12:49:29 +0000

    Version bump for centrifugo

--- plugin/centrifugo/version.php
+++ plugin/centrifugo/version.php
@@ -25,5 +25,5 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component    = 'realtimeplugin_centrifugo';
-$plugin->version      = 2026021900;
+$plugin->version      = 2026022400;
 $plugin->requires     = 2024100700;
